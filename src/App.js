39f9b4e3e7c56aadab92eca6af1bb6b98154d7f0
thomasZhang98 <zhangjiayang@northeastern.edu>
2022-03-20 18:12:26 -0400

    Fix Home

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './vendors/bootstrap/css/bootstrap.min.css';
 import './vendors/bootstrap/bootstrap.min.css';
 import './vendors/fontawesome/css/all.min.css';
-import {BrowserRouter, Routes, Route} from "react-router-dom";
+import {BrowserRouter, Routes, Route, Link} from "react-router-dom";
 import HelloWorld from "./components/HelloWorld";
 import Labs from "./components/Labs";
 import Tuiter from "./components/Tuiter";
@@ -15,6 +15,16 @@ function App() {
           <div className="container">
               <Routes>
                   <Route path="/">
+                      <Route index
+                             element={
+                                 <div>
+                                     <h1>Home</h1>
+                                     <ul>
+                                         <li><Link to={"/labs"}>Labs</Link></li>
+                                         <li><Link to={"/hello"}>Hello World</Link></li>
+                                         <li><Link to={"/tuiter"}>Tuiter</Link></li>
+                                     </ul>
+                                 </div>}/>
                       <Route path="labs"
                              element={<Labs/>}/>
                       <Route path="hello"
--- src/components/Tuiter/NavigationSidebar/index.js
+++ src/components/Tuiter/NavigationSidebar/index.js
@@ -9,8 +9,8 @@ const NavigationSidebar = (
         <>
             <div className="list-group mb-2 mt-2">
                 <i className="list-group-item"><i className="fab fa-twitter"></i></i>
-                <a href="home.html" className={`list-group-item list-group-item-action d-flex align-items-center ${active === 'home'?'active':''}`}><i className="fa-solid fa-house"></i> <span className="d-none d-xl-block ms-1"> Home</span></a>
-                <a href="#" className={`list-group-item list-group-item-action d-flex align-items-center ${active === 'explore'?'active':''}`}><i className="fa-solid fa-hashtag"></i><span className="d-none d-xl-block ms-1"> Explore</span></a>
+                <a href="/tuiter" className={`list-group-item list-group-item-action d-flex align-items-center ${active === 'home'?'active':''}`}><i className="fa-solid fa-house"></i> <span className="d-none d-xl-block ms-1"> Home</span></a>
+                <a href="/tuiter/explore" className={`list-group-item list-group-item-action d-flex align-items-center ${active === 'explore'?'active':''}`}><i className="fa-solid fa-hashtag"></i><span className="d-none d-xl-block ms-1"> Explore</span></a>
                 <a href="notifications.html" className={`list-group-item list-group-item-action d-flex align-items-center ${active === 'notifications'?'active':''}`}><i className="fa-solid fa-bell"></i><span className="d-none d-xl-block ms-1"> Notifications</span></a>
                 <a href="messages.html" className={`list-group-item list-group-item-action d-flex align-items-center ${active === 'messages'?'active':''}`}><i className="fa-solid fa-envelope"></i><span className="d-none d-xl-block ms-1"> Messages</span></a>
                 <a href="bookmarks.html" className={`list-group-item list-group-item-action d-flex align-items-center ${active === 'bookmarks'?'active':''}`}><i className="fa-solid fa-bookmark"></i><span className="d-none d-xl-block ms-1"> Bookmarks</span></a>
